39d06e34f9fbf84da63a24640e223a8c8940e3a9
hrmilan <henriquermilan@gmail.com>
2020-02-05 22:04:42 -0300

    Final functions and comments.

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions defined below will first store a matrix in a
+## cache format and calculate its inverse. The concept is used to
+## reduce time-consuming operations, such as calculating a matrix'
+## inverse, by storing it in cache and retrieving its value.
 
-## Write a short comment describing this function
+## The makeCacheMatrix function creates a matrix that can cache 
+## its inverse. It sets the value of the matrix, gets its value,
+## sets its inverse and gets the value of the inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         m <- NULL
@@ -18,7 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function calculates the inverse matrix stored
+## at the makeCacheMatrix function. If the inverse matrix was 
+## already calculated, it simply retrieves its cached value. If
+## not, it calculates it using the above defined setsolve function.
+
 
 cacheSolve <- function(x, ...) {
         m <- x$getsolve()
